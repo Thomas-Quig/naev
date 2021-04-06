7fd2399bb6e3969d32fb27a636bee739147cfca0
Thomas-Quig <trquig1@gmail.com>
2021-04-06 18:07:20 -0500

    Initial file structure commit

---
+++ src/main.py
@@ -0,0 +1,14 @@
+# Python Imports
+import os
+
+# naev Imports
+
+# 3rd Party Imports
+
+
+
+def main():
+	print("[naev] main() running...")
+
+if __name__ == '__main__':
+	main()
---
+++ src/utils/utils.py
@@ -0,0 +1,5 @@
+def util1():
+	print("Util 1")
+
+def util2():
+	print("Util 2")
